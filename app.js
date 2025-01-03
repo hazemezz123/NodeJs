f34488a7c0e95ebe75e56c1878a05fd9f1c94920
hazemezz123 <hazemezz988@gmail.com>
2025-01-03 16:19:02 +0200

    Lesson 3 , Connect to the database ✅

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
 const port = 3200;
 
-app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
-});
+// MongoDB connection string (use environment variables in production)
+const mongoURI =
+  "mongodb+srv://hazemezz:zv6aulxE5IomPPbi@cluster0.nqqe8.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0";
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    // Start the server after successful MongoDB connection
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+// Serve static files from the "views" directory
+app.use(express.static(path.join(__dirname, "views")));
+
+// Route to serve the index.html file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
